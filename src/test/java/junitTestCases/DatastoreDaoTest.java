80f48f5ca5317d6c5875487075eaae4b8a5471e8
Karthikeyan-Ramesh <karthikeyan15061998@gmail.com>
2020-05-07 22:14:16 +0530

    Commited the working test case for simple datastore creation

--- src/test/java/junitTestCases/DatastoreDaoTest.java
+++ src/test/java/junitTestCases/DatastoreDaoTest.java
@@ -1,6 +1,7 @@
 package junitTestCases;
 
-import static org.mockito.Mockito.when;
+
+import static org.junit.Assert.assertNotEquals;
 
 import java.io.IOException;
 
@@ -8,35 +9,24 @@ import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestC
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.notesapp.daos.DatastoreDao;
 import com.notesapp.pojos.Category;
-import com.notesapp.servlets.CategoryAction;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 public class DatastoreDaoTest {
 
 	
 	 private static final LocalServiceTestHelper helper = new
 	 LocalServiceTestHelper( new LocalDatastoreServiceTestConfig());
 	
-	 private Category catObj;
-	 private CategoryAction catAct;
 	 private DatastoreDao dao;
 	 
-	 @Mock
-	 HttpServletRequest request;
-	 @Mock
-	 MockHttpServletResponse response;
+
 	
 	 @Before
 	 public void setup(){ 
-		 MockitoAnnotations.initMocks(this);
 		 helper.setUp(); 
 		 dao = new DatastoreDao();
 	 }
@@ -50,12 +40,12 @@ public class DatastoreDaoTest {
 	@Test 
 	public void testDatastore()throws ServletException,IOException {
 	
-		when(request.getRequestURI()).thenReturn("http://local/category/add");
-		when(request.getParameter("categoryName")).thenReturn("Sports");
-		when(request.getParameter("createdBy")).thenReturn("Admin");
-		response = new MockHttpServletResponse();
-		catAct.doPost(request, response);
-		Assert.assertEquals("Category created successfully !", response.getWriterContent().toString());
+		Category catObj = new Category.Builder()
+				.categoryName("Sports")
+				.createdBy("Admin")
+				.build();
+		long id = dao.createCategory(catObj);
+		assertNotEquals(0, id);
 	 }
 	 
 }
